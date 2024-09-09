8af6e6ce91cb51b4f0e84d230653a723b50dd1cf
Crivellaro-AEP <aep@assemblaggielettronici.it>
2024-09-09 09:14:48 +0200

    lettore tensioni per andrea update codice

--- EP343_Lettore_tensioni_per_Andrea/src/main.cpp
+++ EP343_Lettore_tensioni_per_Andrea/src/main.cpp
@@ -13,7 +13,7 @@
 #define V3_RANGE_LO 1880
 #define V3_RANGE_HI 1920
 
-int v1, v2, v3;
+int v[3];
 
 void setup()
 {
@@ -26,18 +26,18 @@ void setup()
 
 void loop()
 {
-  v1 = analogRead(IN1);
-  v2 = analogRead(IN2);
-  v3 = analogRead(IN3);
-  printf("____________\nV1: %d\nV2: %d\nV3: %d\n____________\n", v1, v2, v3);
-  printf("V1: %.3f V (approssimato) - ", v1 * VOLT_CONST);
-  printf(v1 < V1_RANGE_HI && v1 > V1_RANGE_LO ? " Valore OK\n" : " Valore fuori scala\n");
+  v[0] = analogRead(IN1);
+  v[1] = analogRead(IN2);
+  v[2] = analogRead(IN3);
+  printf("____________\nV1: %d\nV2: %d\nV3: %d\n____________\n", v[0], v[1], v[2]);
+  printf("V1: %.3f V (approssimato) - ", v[0] * VOLT_CONST);
+  printf(v[0] < V1_RANGE_HI && v[0] > V1_RANGE_LO ? " Valore OK\n" : " Valore fuori scala\n");
 
-  printf("V2: %.3f V (approssimato) - ", v2 * VOLT_CONST);
-  printf(v2 < V2_RANGE_HI && v2 > V2_RANGE_LO ? " Valore OK\n" : " Valore fuori scala\n");
+  printf("V2: %.3f V (approssimato) - ", v[1] * VOLT_CONST);
+  printf(v[1] < V2_RANGE_HI && v[1] > V2_RANGE_LO ? " Valore OK\n" : " Valore fuori scala\n");
 
-  printf("V3: %.3f V (approssimato) - ", v3 * VOLT_CONST);
-  printf(v3 < V3_RANGE_HI && v3 > V3_RANGE_LO ? " Valore OK\n" : " Valore fuori scala\n");
+  printf("V3: %.3f V (approssimato) - ", v[2] * VOLT_CONST);
+  printf(v[2] < V3_RANGE_HI && v[2] > V3_RANGE_LO ? " Valore OK\n" : " Valore fuori scala\n");
 
   delay(1000);
 }
